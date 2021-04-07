c73325ac86cce921bde9cf57996113e4587f7ad5
ivanpetrovic2792 <piwke.kkbask@gmail.com>
2021-04-07 19:41:13 +0200

    Kreiranje baze i početne aplikacije

---
+++ M8Find.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using M8Find.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace M8Find.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ M8Find.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace M8Find.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
